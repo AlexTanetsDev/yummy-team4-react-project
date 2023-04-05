28f73359353a1b045411bdc4fae3179b8f480a7c
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2023-04-05 20:03:15 +0200

    Create section
    create component Sections, add style and responsive

---
+++ src/components/Sections/Sections.styled.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  padding-top: ${p => p.theme.space(12.5)};
+  padding-bottom: ${p => p.theme.space(25)};
+
+  @media (min-width: 768px) {
+    padding-top: ${p => p.theme.space(18)};
+    padding-bottom: ${p => p.theme.space(50)};
+  }
+  @media (min-width: 1440px) {
+    padding-top: ${p => p.theme.space(25)};
+  }
+`;
+
+export const SectionTitle = styled.title`
+  display: inline-block;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-size: ${p => p.theme.fontSizes.xxxl}px;
+  line-height: 28px;
+  letter-spacing: -0.02em;
+  font-feature-settings: 'liga' off;
+  color: ${p => p.theme.colors.mainTitleText};
+  margin-bottom: 50px;
+
+  @media (min-width: 768px) {
+    font-size: 32px;
+    line-height: 32px;
+  }
+
+  @media (min-width: 1440px) {
+    font-size: ${p => p.theme.fontSizes.xxxxl}px;
+    line-height: 44px;
+    margin-bottom: 100px;
+  }
+`;
