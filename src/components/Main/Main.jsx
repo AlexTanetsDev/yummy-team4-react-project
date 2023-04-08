3caadae09ca3d356f54b0b50ac6fad02ddb4e26d
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2023-04-08 12:45:18 +0300

    add container to Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 import { ChoseYourBreakfast } from 'components/ChoseYourBreakfast/ChoseYourBreakfast';
 import { Search } from 'components/Search/Search';
 import { PreviewCategories } from 'components/PreviewCategories/PreviewCategories';
+import { Container } from 'components/Container/Container';
+// import { Container } from 'components/Container/Container';
 
 export const Main = () => {
   return (
     <div>
-      <ChoseYourBreakfast />
-      <Search />
-      <PreviewCategories />
+      <Container children>
+        <ChoseYourBreakfast />
+        <Search />
+        <PreviewCategories />
+      </Container>
     </div>
   );
 };
--- src/components/PreviewCategories/PreviewCategories.jsx
+++ src/components/PreviewCategories/PreviewCategories.jsx
@@ -1,19 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useAuth } from '../../hooks';
+import { useSelector } from 'react-redux';
+// import { useAuth } from '../../hooks';
 import axios from 'axios';
-
+import { Sections } from 'components/Sections/Sections';
 import { SeeAllButton } from 'components/Button/Button.jsx';
 
 // axios.defaults.headers.common['Authorization'] =
 //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDJjOGU1NmY3M2E0YmE5ZDlhNzk4MTYiLCJpYXQiOjE2ODA5MDY4MTYsImV4cCI6MTY4MDk4OTYxNn0.cOeYg8Y4WvI_KfS6fYlFDxSniwl6yVjnNRcKWjTD15U';
 // 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDJjOGU1NmY3M2E0YmE5ZDlhNzk4MTYiLCJpYXQiOjE2ODA5NDE3OTksImV4cCI6MTY4MTAyNDU5OX0.R7puNattOS6LksGKc-OmhrSfN95oCp2yHJ1YPfb97D8',
-
-const instance = axios.create({
-  baseURL: 'https://yummy-team4-nodejs-project.onrender.com/api',
-  headers: {
-    Authorization: `Bearer ${useAuth.token}`,
-  },
-});
 
 function onHandlCklik() {
   console.log(3);
@@ -21,11 +15,20 @@ function onHandlCklik() {
 
 export function PreviewCategories() {
   const [items, setItems] = useState('');
+  const token = useSelector(state => state.auth.token);
+
+  const instance = axios.create({
+    baseURL: 'https://yummy-team4-nodejs-project.onrender.com/api',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
   useEffect(() => {
     instance.get('/recipes/main-page').then(function (response) {
       setItems(response.data);
     });
-  }, []);
+  }, [instance]);
 
   return (
     <>
@@ -35,8 +38,7 @@ export function PreviewCategories() {
               const { category, recipes } = item;
               return (
                 <ul key={category}>
-                  <li>{category}</li>
-                  <li>
+                  <Sections title={category} children>
                     {recipes
                       .map(recipe => {
                         const { preview, title } = recipe;
@@ -50,8 +52,8 @@ export function PreviewCategories() {
                         );
                       })
                       .slice(0, 4)}
-                  </li>
-                  <SeeAllButton onClick={onHandlCklik} children="See all" />
+                    <SeeAllButton onClick={onHandlCklik} children="See all" />
+                  </Sections>
                 </ul>
               );
             })
