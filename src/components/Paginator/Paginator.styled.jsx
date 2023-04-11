62f100525c65106feab196cf5a96210acfa1143d
KovalchukAnastasia <103355891+KovalchukAnastasia@users.noreply.github.com>
2023-04-11 15:38:21 +0300

    add_styles

--- src/components/Paginator/Paginator.styled.jsx
+++ src/components/Paginator/Paginator.styled.jsx
@@ -1,16 +1,20 @@
 import styled from 'styled-components';
 export const PaginationWrapper = styled.div`
-  margin-top: 50px;
+  margin-top: 40px;
   display: flex;
   justify-content: center;
   align-items: center;
+  @media (min-width: 768px) {
+    margin-top: 50px;
+  }
 `;
 
 export const PaginationBtn = styled.button`
-  color: #8baa36;
-  font-size: 14px;
+  color: ${p => p.theme.colors.firstAccentColor};
+  font-family: 'Poppins';
+  font-size: ${p => p.theme.fontSizes.s}px;
   margin: 10px;
   padding: 3px 8px;
-  border: 2px solid #8baa36;
+  border: ${p => p.theme.border.greenBtnBorder};
   border-radius: 3px;
 `;
