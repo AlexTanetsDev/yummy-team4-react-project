239f33b0521b01721eb54e15cffe3d8a96987b0e
Roman Stelmakh <105305762+Roman0407@users.noreply.github.com>
2023-04-11 02:43:55 +0200

    fix footer

--- src/components/Footer/LogoFooter/LogoFooter.jsx
+++ src/components/Footer/LogoFooter/LogoFooter.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Logo from '../../Logo/Logo';
+import Logo from 'react-icons';
 import { LogoWrapper, Title } from './LogoFooter.styled';
 
 const LogoFooter = () => {
