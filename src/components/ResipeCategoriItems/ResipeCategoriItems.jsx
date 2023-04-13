26b9fd69d778b118e07b2a4f0a6f269e9b696471
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2023-04-13 18:29:58 +0200

    Category
    Add isLoading and error
    Create function titleLength - checked length of title if length longer of 40 simbols chenged length

--- src/components/ResipeCategoriItems/ResipeCategoriItems.jsx
+++ src/components/ResipeCategoriItems/ResipeCategoriItems.jsx
@@ -7,18 +7,29 @@ import {
 } from './ResipeCategoriItems.styled';
 
 const ResipeCategoriItems = ({ recipes }) => {
+  const titleLength = title => {
+    if (title.length >= 40) {
+      const shortTitle = title.split(' ').slice(0, 5).join(' ') + '...';
+      return shortTitle;
+    }
+    return title;
+  };
+
   return (
     <>
       <RecipesContainer>
         {recipes.length !== 0 &&
-          recipes.map(({ _id: id, title, thumb }) => (
-            <RecipeItem key={id}>
-              <Link to={`/recipe/${id}`}>
-                <ImgRecipe src={thumb} alt={title} />
-                <TitleRecipe>{title}</TitleRecipe>
-              </Link>
-            </RecipeItem>
-          ))}
+          recipes.map(({ _id: id, title, thumb }) => {
+            const shortTitle = titleLength(title);
+            return (
+              <RecipeItem key={id}>
+                <Link to={`/recipe/${id}`}>
+                  <ImgRecipe src={thumb} alt={title} />
+                  <TitleRecipe>{shortTitle}</TitleRecipe>
+                </Link>
+              </RecipeItem>
+            );
+          })}
       </RecipesContainer>
     </>
   );
