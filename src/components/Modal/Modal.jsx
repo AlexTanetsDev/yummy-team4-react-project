970d5fd6e4e29c14dc7ee0c827a45d403fca5d6d
Tanets Olexandr <104691488+AlexTanetsDev@users.noreply.github.com>
2023-04-14 17:15:52 +0300

    Revert "fixes"

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,86 +4,81 @@ import {
   MobileMenu,
   StyledNavBurger,
   StyledLinkBurger,
+  ThemeModalDiv,
+  StyledThemeBtn,
   Content,
   StyledBurger,
-  ModalHeader,
+ ModalHeader
 } from './ModalStyled';
-import { SearchIconMobile } from '../Header/Header.styled';
+import {SearchIcon} from "../Header/Header.styled"
+import Dark from '../../images/Switch-dark.svg';
+import Light from '../../images/Switch-light.svg';
 
+import { useState } from 'react';
 import { LogoHeader } from 'components/Logo/Logo';
 import { IoMdClose } from 'react-icons/io';
 
 export const Modal = ({ active, setActive }) => {
+  const [theme, setTheme] = useState(false);
+  const handleTheme = () => {
+    setTheme(!theme);
+  };
+
   return (
-    <MobileMenu
+  
+          <MobileMenu
       className={active ? 'modal active' : 'modal'}
       onClick={() => {
         setActive(false);
       }}
     >
-      <ModalHeader>
-        <LogoHeader />
-        <StyledBurger
-          onClick={() => {
-            setActive(false);
-          }}
-          // onClick={handleButtonClick}
-        >
-          <IoMdClose style={{ width: '32px', height: '32px' }} />
-        </StyledBurger>
+    <ModalHeader>
+      <LogoHeader />
+      <StyledBurger
+        onClick={() => {
+          setActive(false);
+        }}
+        // onClick={handleButtonClick}
+      >
+        <IoMdClose style={{ width: '32px', height: '32px' }} />
+      </StyledBurger>
       </ModalHeader>
+      
       <Content className="modal_content" onClick={evt => evt.stopPropagation()}>
         <StyledNavBurger>
-          <StyledLinkBurger
-            to="/categories/Beef"
-            onClick={() => {
-              setActive(false);
-            }}
-          >
-            Categories
-          </StyledLinkBurger>
-          <StyledLinkBurger
-            to="/add"
-            onClick={() => {
-              setActive(false);
-            }}
-          >
-            Add recipes
-          </StyledLinkBurger>
-          <StyledLinkBurger
-            to="/my"
-            onClick={() => {
-              setActive(false);
-            }}
-          >
-            My recipes
-          </StyledLinkBurger>
-          <StyledLinkBurger
-            to="/favorite"
-            onClick={() => {
-              setActive(false);
-            }}
-          >
-            Favorites
-          </StyledLinkBurger>
-          <StyledLinkBurger
-            to="/shopping-list"
-            onClick={() => {
-              setActive(false);
-            }}
-          >
-            Shopping list
-          </StyledLinkBurger>
-          <StyledLinkBurger
-            to="/search"
-            onClick={() => {
-              setActive(false);
-            }}
-          >
-            <SearchIconMobile /> Search
+          <StyledLinkBurger to="/categories/Beef" onClick={() => {
+        setActive(false);
+      }}>Categories</StyledLinkBurger>
+          <StyledLinkBurger to="/add" onClick={() => {
+        setActive(false);
+      }}>Add recipes</StyledLinkBurger>
+          <StyledLinkBurger to="/my" onClick={() => {
+        setActive(false);
+      }}>My recipes</StyledLinkBurger>
+          <StyledLinkBurger to="/fovorites" onClick={() => {
+        setActive(false);
+      }}>Favorites</StyledLinkBurger>
+          <StyledLinkBurger to="/shopping-list" onClick={() => {
+        setActive(false);
+      }}>Shopping list</StyledLinkBurger>
+          <StyledLinkBurger to="/search" onClick={() => {
+        setActive(false);
+      }}>
+            <SearchIcon /> Search
           </StyledLinkBurger>
         </StyledNavBurger>
-      </Content>
-    </MobileMenu>
+        <ThemeModalDiv>
+          <StyledThemeBtn onClick={handleTheme}>
+            {theme ? (
+              <img src={Dark} alt="theme" />
+            ) : (
+              <img src={Light} alt="theme" />
+            )}
+          </StyledThemeBtn>
+        </ThemeModalDiv>
+        </Content>
+       
+      </MobileMenu>
+   
   );
 };
--- src/components/Modal/ModalStyled.js
+++ src/components/Modal/ModalStyled.js
@@ -1,33 +1,9 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
-import { modal, modalMini } from '../../images/index';
+import { modal } from '../../images/index';
 
 export const MobileMenu = styled.div`
-  @media screen and (max-width: 767px) {
-    position: fixed;
-    height: 100vh;
-    width: 100vw;
-    top: 0;
-    left: 0;
-    padding-top: 18px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    transform: scale(0);
-    background-image: url(${modalMini});
-    background-color: #ebf3d4;
-    background-repeat: no-repeat;
-    background-position: bottom right;
-
-    &::before {
-      content: '';
-      position: absolute;
-      background-image: inherit;
-      transform: rotate(100deg);
-    }
-  }
-
-  @media screen and (min-width: 768px) and (max-width: 1443px) {
+  @media screen and (max-width: 1440px) {
     position: fixed;
     height: 100vh;
     width: 100vw;
@@ -50,7 +26,6 @@ export const MobileMenu = styled.div`
       transform: rotate(100deg);
     }
   }
-
   display: none;
 `;
 
@@ -65,20 +40,11 @@ export const ModalHeader = styled.div`
 export const StyledLinkBurger = styled(NavLink)`
   font-family: 'Poppins';
   font-style: normal;
-  font-weight: 600;
-  font-size: 18px;
-  line-height: 18px;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 24px;
   letter-spacing: -0.02em;
   color: #22252a;
-
-  @media screen and (min-width: 768px) {
-    font-size: 24px;
-    line-height: 24px;
-  }
-
-  &:last-of-type {
-    display: flex;
-  }
 
   &.active {
     color: #8baa36;
@@ -121,7 +87,6 @@ export const Content = styled.div`
 `;
 
 export const StyledBurger = styled.button`
-  position: absolute;
   margin-left: auto;
   top: 0;
   right: ${p => p.theme.space(6)};
