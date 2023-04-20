2e5053784320b2626eac358e70b8c8ffc1dda57b
SerhiiSamara <ndaair@gmail.com>
2023-04-20 18:16:06 +0300

    Styled component

--- src/components/SigninForm/SigninForm.jsx
+++ src/components/SigninForm/SigninForm.jsx
@@ -3,8 +3,6 @@ import { Formik, Form, Field } from 'formik';
 import { useDispatch } from 'react-redux';
 import { categoryList, signIn } from '../../Redux/auth/operations';
 import { object, string } from 'yup';
-import { FiMail } from 'react-icons/fi';
-import { FiLock } from 'react-icons/fi';
 import { SingInButtonGreen } from 'components/Button/Button';
 import {
   StyledWrapper,
@@ -18,7 +16,10 @@ import {
   Link,
   IconWrap,
 	InputWrapper,
-	ContentWrapper
+	ContentWrapper,
+	StyledFiMail,
+	StyledFiLock,
+	WarningAndSuccessMessage,
 } from './SignInForm.styled';
 import '../../styles/styles.css';
 import { FormError } from 'components/FormError/FormError';
@@ -80,40 +81,20 @@ export const SignInForm = () => {
                     <Form>
                       <InputWrapper>
                         <Field
-                          as={InputField}
-                          className={`sign-up__inp ${
-                            errors.email && touched.email
-                              ? 'sign-up__inp_error'
-                              : ''
-                          } ${
-                            !errors.email && touched.email
-                              ? 'sign-up__inp_suc'
-                              : ''
-                          }`}	
+													as={InputField}
+													brdcolor={(!touched.email && 'white') || (errors.email && touched.email && '#e74a3b') || (!errors.email && touched.email && '#3cbc81')}																																		
                           name="email"
                           type="text"
                           placeholder="Email"
                           values={values.email}
                         />
                         <IconWrap>
-                          {window.innerWidth < 768 && (
-                            <FiMail
+                            <StyledFiMail
                               color={`${(!touched.email && 'white') ||
 																				(errors.email && touched.email && '#e74a3b') ||
 																				(!errors.email && touched.email && '#3cbc81')
                               }`}
-                              size={20}
                             />
-                          )}
-                          {window.innerWidth >= 768 && (
-                            <FiMail
-                              color={`${(!touched.email && 'white') ||
-																				(errors.email && touched.email && '#e74a3b') ||
-																				(!errors.email && touched.email && '#3cbc81')
-                              }`}
-                              size={24}
-                            />
-                          )}
                         </IconWrap>
                         {errors.email && touched.email && (
                           <StateInputIcon src={errorIcon} />
@@ -125,49 +106,22 @@ export const SignInForm = () => {
                       </InputWrapper>
                       <InputWrapper>
                         <Field
-                          as={InputField}
-                          className={`sign-up__inp ${
-                            errors.password && touched.password
-                              ? 'sign-up__inp_error'
-                              : ''
-                          } ${
-                            0 < values.password.length &&
-                            values.password.length < 8 &&
-                            !errors.password
-                              ? 'sign-up__inp_war'
-                              : ''
-                          } ${
-                            8 <= values.password.length && !errors.password
-                              ? 'sign-up__inp_suc'
-                              : ''
-                          }`}		
+													as={InputField}
+													brdcolor={(!touched.password && 'white') || (errors.password && touched.password && '#e74a3b') || (6 <= values.password.length && values.password.length < 8 && '#f6c23e') ||
+														(8 <= values.password.length && '#3cbc81')}																		
                           type="password"
                           name="password"
                           placeholder="Password"
                         />
                         <IconWrap>
-                          {window.innerWidth < 768 && (
-                            <FiLock
-                              className="input-icon__mobile"
+                            <StyledFiLock
                               color={`${(!touched.password && 'white') ||
-																				(errors.password && touched.password && '#e74a3b') ||
-																        (6 <= values.password.length && values.password.length < 8 && '#f6c23e') ||
-																        (8 <= values.password.length && '#3cbc81')
+																(errors.password && touched.password && '#e74a3b') ||
+																(6 <= values.password.length && values.password.length < 8 && '#f6c23e') ||
+																(8 <= values.password.length && '#3cbc81')
                                     }`}
                               size={20}
                             />
-                          )}
-                          {window.innerWidth >= 768 && (
-                            <FiLock
-                              className="input-con"
-                              color={`${(!touched.password && 'white') ||
-																				(errors.password && touched.password && '#e74a3b') ||
-																        (6 <= values.password.length && values.password.length < 8 && '#f6c23e') ||
-																        (8 <= values.password.length && '#3cbc81')
-                                    }`}
-                              size={24}
-                            />
-                          )}
                         </IconWrap>
                         {6 <= values.password.length &&
                           values.password.length < 8 && !errors.password &&
@@ -177,17 +131,17 @@ export const SignInForm = () => {
                         {6 <= values.password.length &&
                           values.password.length < 8 && !errors.password &&
                            (
-                            <p className="sing-up__inp_war-mess">
+                            <WarningAndSuccessMessage color={'#f6c23e'}>
                               Your password is little secure
-                            </p>
+                            </WarningAndSuccessMessage>
                           )}
                         {8 <= values.password.length && !errors.password && (
                           <StateInputIcon src={succesIcon} />
                         )}
                         {8 <= values.password.length && !errors.password && (
-                          <p className="sing-up__inp_suc-mess">
+                          <WarningAndSuccessMessage color={'#3cbc81'}>
                             Password is secure
-                          </p>
+                          </WarningAndSuccessMessage>
                         )}
                         {errors.password && touched.password && (
                           <StateInputIcon src={errorIcon} />
