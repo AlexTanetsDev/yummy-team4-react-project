0a68e6a490cb9f8a459473c640c2f66a3f305b63
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2023-04-24 18:48:38 +0200

    categoryPagination
    create pagination for page Category

--- src/apiService/getCategory.js
+++ src/apiService/getCategory.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
 
-export const getRecipesByCategory = async (category, token) => {
+export const getRecipesByCategory = async (
+  category,
+  token,
+  page = 1,
+  limit = 8
+) => {
   try {
-    const respons = await axios.get(`/api/recipes/category/${category}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const respons = await axios.get(
+      `/api/recipes/category/${category}?page=${page}&limit=${limit}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
     return respons.data;
   } catch (error) {
     return error.message;
