e341b2b99600d0459356a4bbea6a3444b816721c
SerhiiSamara <ndaair@gmail.com>
2023-05-30 23:18:34 +0300

    Rewright authSlice, EmailVerify, RegisterForm, SigninForm

--- src/Redux/auth/authSlise.js
+++ src/Redux/auth/authSlise.js
@@ -7,14 +7,17 @@ import {
   categoryList,
   updateUser,
   verify,
+  register,
 } from './operations';
 
 const initialState = {
   user: { name: null, email: null, avatarURl: '', verify: false },
   token: null,
   isLoggedIn: false,
+  isLoading: false,
   isRefreshing: false,
   categoryList: [],
+  error: null,
 };
 
 const authSlice = createSlice({
@@ -22,22 +25,59 @@ const authSlice = createSlice({
   initialState,
   extraReducers: builder =>
     builder
+      .addCase(verify.pending, state => {
+        state.isLoading = true;
+      })
       .addCase(verify.fulfilled, (state, action) => {
         state.user = action.payload.user;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(verify.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(register.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(signIn.pending, state => {
+        state.isLoading = true;
       })
       .addCase(signIn.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
+        state.isLoading = false;
         state.isLoggedIn = true;
+        state.error = null;
       })
-      .addCase(signIn.rejected, state => {
+      .addCase(signIn.rejected, (state, action) => {
         state.token = null;
         state.isLoggedIn = false;
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(logOut.pending, state => {
+        state.isLoading = true;
       })
       .addCase(logOut.fulfilled, state => {
         state.user = { name: null, email: null, avatarURL: '' };
         state.token = null;
         state.isLoggedIn = false;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(logOut.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
       })
       .addCase(refreshUser.pending, state => {
         state.isRefreshing = true;
@@ -46,19 +86,45 @@ const authSlice = createSlice({
         state.user = action.payload;
         state.isLoggedIn = true;
         state.isRefreshing = false;
+        state.error = null;
       })
-      .addCase(refreshUser.rejected, state => {
+      .addCase(refreshUser.rejected, (state, action) => {
         state.isRefreshing = false;
+        state.error = action.payload;
+      })
+      .addCase(updateSubscription.pending, state => {
+        state.isRefreshing = true;
       })
       .addCase(updateSubscription.fulfilled, (state, action) => {
         state.user.subscription = action.payload.subscription;
+        state.error = null;
+      })
+      .addCase(updateSubscription.rejected, (state, action) => {
+        state.isRefreshing = false;
+        state.error = action.payload;
+      })
+      .addCase(categoryList.pending, state => {
+        state.isRefreshing = true;
       })
       .addCase(categoryList.fulfilled, (state, action) => {
         state.categoryList = action.payload;
+        state.error = null;
+      })
+      .addCase(categoryList.rejected, (state, action) => {
+        state.isRefreshing = false;
+        state.error = action.payload;
+      })
+      .addCase(updateUser.pending, state => {
+        state.isRefreshing = true;
       })
       .addCase(updateUser.fulfilled, (state, action) => {
         state.user.avatarURL = action.payload.avatarURL;
         state.user.name = action.payload.name;
+        state.error = null;
+      })
+      .addCase(updateUser.rejected, (state, action) => {
+        state.isRefreshing = false;
+        state.error = action.payload;
       }),
 });
 
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { Formik, Form, Field } from 'formik';
 import { object, string } from 'yup';
@@ -8,8 +8,8 @@ import { useTranslation } from 'react-i18next';
 import { SingInButtonGreen } from 'components/Button/Button';
 import { FormError } from 'components/FormError/FormError';
 import { MainLoader } from 'components/Loader/Loader';
-import { AlertMessage } from 'components/AlertMessage/AlertMessage';
 import { LanguageSelector } from 'components/LanguageSelector/LanguageSelector';
+import { selectError, selectIsLoading } from 'Redux/auth/selectors';
 
 import { register } from 'Redux/auth/operations';
 
@@ -50,23 +50,18 @@ const registerSchema = object({
 });
 
 export const RegisterForm = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
   const dispatch = useDispatch();
   const { t } = useTranslation();
 
   const [type, setType] = useState('password');
   const [toggleIcon, setToggleIcon] = useState(<StyledAiFillEyeInvisible />);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   const handleSubmit = async (values, { resetForm }) => {
-    try {
-      setIsLoading(true);
-      await dispatch(register(values));
+    await dispatch(register(values));
+    if (!error) {
       resetForm();
-    } catch (error) {
-      setError(error);
-    } finally {
-      setIsLoading(false);
     }
   };
 
@@ -82,166 +77,154 @@ export const RegisterForm = () => {
 
   return ReactDOM.createPortal(
     <>
-      {error ? (
-        <AlertMessage>
-          Oops, something went wrong. Please try again later...
-        </AlertMessage>
+      {isLoading ? (
+        <MainLoader />
       ) : (
-        <>
-          {isLoading ? (
-            <MainLoader />
-          ) : (
-            <StyledWrapper>
-              <LanguageSelector page="auth" />
-              <BottomBgImage />
-              <ContentWrapper>
-                <ImageReg />
-                <ModalWrapper>
-                  <Modal>
-                    <Title>{t('Registration')}</Title>
-                    <Formik
-                      initialValues={initialValues}
-                      validationSchema={registerSchema}
-                      onSubmit={handleSubmit}
-                    >
-                      {({ values, errors, touched }) => (
-                        <Form autoComplete="off">
-                          <InputWrapper>
-                            <Field
-                              as={InputField}
-                              brdcolor={
-                                (!touched.name && 'white') ||
-                                (errors.name && touched.name && '#e74a3b') ||
-                                (!errors.name && touched.name && '#3cbc81')
-                              }
-                              type="text"
-                              name="name"
-                              placeholder={t('Name')}
-                              values={values.name}
-                            />
-                            <StyledAiOutlineUser
-                              color={`${
-                                (!touched.name && 'white') ||
-                                (errors.name && touched.name && '#e74a3b') ||
-                                (!errors.name && touched.name && '#3cbc81')
-                              }`}
-                            />
-                            <IconsWrap>
-                              {errors.name && touched.name && (
-                                <StateInputIcon src={errorIcon} />
-                              )}
-                              {!errors.name && touched.name && (
-                                <StateInputIcon src={succesIcon} />
-                              )}
-                            </IconsWrap>
-                            <FormError name="name" component="div" />
-                          </InputWrapper>
-                          <InputWrapper>
-                            <Field
-                              as={InputField}
-                              brdcolor={
-                                (!touched.email && 'white') ||
-                                (errors.email && touched.email && '#e74a3b') ||
-                                (!errors.email && touched.email && '#3cbc81')
-                              }
-                              name="email"
-                              type="email"
-                              inputmode="email"
-                              placeholder={t('Email')}
-                              values={values.email}
-                            />
-                            <StyledFiMail
-                              color={`${
-                                (!touched.email && 'white') ||
-                                (errors.email && touched.email && '#e74a3b') ||
-                                (!errors.email && touched.email && '#3cbc81')
-                              }`}
-                            />
-                            <IconsWrap>
-                              {errors.email && touched.email && (
-                                <StateInputIcon src={errorIcon} />
-                              )}
-                              {!errors.email && touched.email && (
-                                <StateInputIcon src={succesIcon} />
-                              )}
-                            </IconsWrap>
-                            <FormError name="email" component="div" />
-                          </InputWrapper>
-                          <InputWrapper>
-                            <Field
-                              type={type}
-                              name="password"
-                              placeholder={t('Password')}
-                              values={values.password}
-                              as={InputField}
-                              brdcolor={
-                                (!touched.password && 'white') ||
-                                (errors.password &&
-                                  touched.password &&
-                                  '#e74a3b') ||
-                                (6 <= values.password.length &&
-                                  values.password.length < 8 &&
-                                  '#f6c23e') ||
-                                (8 <= values.password.length && '#3cbc81')
-                              }
-                            />
-                            <StyledFiLock
-                              color={`${
-                                (!touched.password && 'white') ||
-                                (errors.password &&
-                                  touched.password &&
-                                  '#e74a3b') ||
-                                (6 <= values.password.length &&
-                                  values.password.length < 8 &&
-                                  '#f6c23e') ||
-                                (8 <= values.password.length && '#3cbc81')
-                              }`}
-                            />
-                            <IconsWrap>
-                              {6 <= values.password.length &&
-                                values.password.length < 8 &&
-                                !errors.password && (
-                                  <StateInputIcon src={warningIcon} />
-                                )}
-                              {8 <= values.password.length &&
-                                !errors.password && (
-                                  <StateInputIcon src={succesIcon} />
-                                )}
-                              {errors.password && touched.password && (
-                                <StateInputIcon src={errorIcon} />
-                              )}
-                              <EyeIcon onClick={handleClick}>
-                                {toggleIcon}
-                              </EyeIcon>
-                            </IconsWrap>
-                            {6 <= values.password.length &&
+        <StyledWrapper>
+          <LanguageSelector page="auth" />
+          <BottomBgImage />
+          <ContentWrapper>
+            <ImageReg />
+            <ModalWrapper>
+              <Modal>
+                <Title>{t('Registration')}</Title>
+                <Formik
+                  initialValues={initialValues}
+                  validationSchema={registerSchema}
+                  onSubmit={handleSubmit}
+                >
+                  {({ values, errors, touched }) => (
+                    <Form autoComplete="off">
+                      <InputWrapper>
+                        <Field
+                          as={InputField}
+                          brdcolor={
+                            (!touched.name && 'white') ||
+                            (errors.name && touched.name && '#e74a3b') ||
+                            (!errors.name && touched.name && '#3cbc81')
+                          }
+                          type="text"
+                          name="name"
+                          placeholder={t('Name')}
+                          values={values.name}
+                        />
+                        <StyledAiOutlineUser
+                          color={`${
+                            (!touched.name && 'white') ||
+                            (errors.name && touched.name && '#e74a3b') ||
+                            (!errors.name && touched.name && '#3cbc81')
+                          }`}
+                        />
+                        <IconsWrap>
+                          {errors.name && touched.name && (
+                            <StateInputIcon src={errorIcon} />
+                          )}
+                          {!errors.name && touched.name && (
+                            <StateInputIcon src={succesIcon} />
+                          )}
+                        </IconsWrap>
+                        <FormError name="name" component="div" />
+                      </InputWrapper>
+                      <InputWrapper>
+                        <Field
+                          as={InputField}
+                          brdcolor={
+                            (!touched.email && 'white') ||
+                            (errors.email && touched.email && '#e74a3b') ||
+                            (!errors.email && touched.email && '#3cbc81')
+                          }
+                          name="email"
+                          type="email"
+                          inputmode="email"
+                          placeholder={t('Email')}
+                          values={values.email}
+                        />
+                        <StyledFiMail
+                          color={`${
+                            (!touched.email && 'white') ||
+                            (errors.email && touched.email && '#e74a3b') ||
+                            (!errors.email && touched.email && '#3cbc81')
+                          }`}
+                        />
+                        <IconsWrap>
+                          {errors.email && touched.email && (
+                            <StateInputIcon src={errorIcon} />
+                          )}
+                          {!errors.email && touched.email && (
+                            <StateInputIcon src={succesIcon} />
+                          )}
+                        </IconsWrap>
+                        <FormError name="email" component="div" />
+                      </InputWrapper>
+                      <InputWrapper>
+                        <Field
+                          type={type}
+                          name="password"
+                          placeholder={t('Password')}
+                          values={values.password}
+                          as={InputField}
+                          brdcolor={
+                            (!touched.password && 'white') ||
+                            (errors.password &&
+                              touched.password &&
+                              '#e74a3b') ||
+                            (6 <= values.password.length &&
                               values.password.length < 8 &&
-                              !errors.password && (
-                                <WarningAndSuccessMessage color={'#f6c23e'}>
-                                  {t('Your password is little secure')}
-                                </WarningAndSuccessMessage>
-                              )}
-                            {8 <= values.password.length &&
-                              !errors.password && (
-                                <WarningAndSuccessMessage color={'#3cbc81'}>
-                                  {t('Password is secure')}
-                                </WarningAndSuccessMessage>
-                              )}
-                            <FormError name="password" component="div" />
-                          </InputWrapper>
-                          <SingInButtonGreen type="submit">
-                            {t('Sign up')}
-                          </SingInButtonGreen>
-                        </Form>
-                      )}
-                    </Formik>
-                  </Modal>
-                  <Link to="/signin">{t('Sign in')}</Link>
-                </ModalWrapper>
-              </ContentWrapper>
-            </StyledWrapper>
-          )}
-        </>
+                              '#f6c23e') ||
+                            (8 <= values.password.length && '#3cbc81')
+                          }
+                        />
+                        <StyledFiLock
+                          color={`${
+                            (!touched.password && 'white') ||
+                            (errors.password &&
+                              touched.password &&
+                              '#e74a3b') ||
+                            (6 <= values.password.length &&
+                              values.password.length < 8 &&
+                              '#f6c23e') ||
+                            (8 <= values.password.length && '#3cbc81')
+                          }`}
+                        />
+                        <IconsWrap>
+                          {6 <= values.password.length &&
+                            values.password.length < 8 &&
+                            !errors.password && (
+                              <StateInputIcon src={warningIcon} />
+                            )}
+                          {8 <= values.password.length && !errors.password && (
+                            <StateInputIcon src={succesIcon} />
+                          )}
+                          {errors.password && touched.password && (
+                            <StateInputIcon src={errorIcon} />
+                          )}
+                          <EyeIcon onClick={handleClick}>{toggleIcon}</EyeIcon>
+                        </IconsWrap>
+                        {6 <= values.password.length &&
+                          values.password.length < 8 &&
+                          !errors.password && (
+                            <WarningAndSuccessMessage color={'#f6c23e'}>
+                              {t('Your password is little secure')}
+                            </WarningAndSuccessMessage>
+                          )}
+                        {8 <= values.password.length && !errors.password && (
+                          <WarningAndSuccessMessage color={'#3cbc81'}>
+                            {t('Password is secure')}
+                          </WarningAndSuccessMessage>
+                        )}
+                        <FormError name="password" component="div" />
+                      </InputWrapper>
+                      <SingInButtonGreen type="submit">
+                        {t('Sign up')}
+                      </SingInButtonGreen>
+                    </Form>
+                  )}
+                </Formik>
+              </Modal>
+              <Link to="/signin">{t('Sign in')}</Link>
+            </ModalWrapper>
+          </ContentWrapper>
+        </StyledWrapper>
       )}
     </>,
     document.querySelector('#modal-root')
